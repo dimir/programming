79edd1f2ae0979566781df399bd9994c96f918f6
Vladimir Levijev <vl@dimir.eu>
2018-06-20 15:36:50 +0300

    upgrade-skype.sh: remove temporary file if upgrade not needed
    Signed-off-by: Vladimir Levijev <vl@dimir.eu>

--- shell/upgrade-skype.sh
+++ shell/upgrade-skype.sh
@@ -2,12 +2,14 @@
 
 PKG_NAME="skypeforlinux"
 PKG_FILE="${PKG_NAME}-64.deb"
+PKG_PATH="/tmp/${PKG_FILE}"
 
-wget -q --show-progress https://go.skype.com/$PKG_FILE -O $PKG_FILE || exit 1
+wget -q --show-progress https://go.skype.com/$PKG_FILE -O $PKG_PATH || exit 1
 current=$(dpkg-query --showformat='${Version}' --show $PKG_NAME 2>/dev/null)
-update=$(dpkg-deb -f $PKG_FILE Version)
+update=$(dpkg-deb -f $PKG_PATH Version)
 if [[ -n "$current" && "$current" = "$update" ]]; then
 	echo "Installed version is already latest: $current"
+	rm -f $PKG_PATH
 	exit
 fi
 echo -en "current\t: "
@@ -17,6 +19,5 @@ echo -n "Upgrade? [Y/n] "
 read ans
 [ -z "$ans" ] && ans="y" || ans=$(echo $ans | tr '[A-Z]' '[a-z]')
 [ "$ans" = "y" ] || exit 1
-pkill skypeforlinux || exit 1
-sudo dpkg -i $PKG_FILE || exit 1
-rm -f $PKG_FILE
+sudo dpkg -i $PKG_PATH || exit 1
+rm -f $PKG_PATH
